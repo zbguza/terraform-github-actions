8dedbb64b489b0d27526e8d0184ebcacbd265666
zbguza <104495309+zbguza@users.noreply.github.com>
2024-06-06 15:06:09 +0200

    test

--- main.tf
+++ main.tf
@@ -13,13 +13,13 @@ terraform {
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc             = true
-    use_azuread_auth     = true
   }
 }
 
 provider "azurerm" {
   features {}
   use_oidc = true
+  use_azuread_auth = true
 }
 
 # Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
